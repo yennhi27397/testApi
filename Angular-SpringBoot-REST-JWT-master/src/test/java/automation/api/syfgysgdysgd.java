411be2f873d13574d9acb2f40ae83e68ca0c4844
yennhi27397 <137993057+yennhi27397@users.noreply.github.com>
2023-07-25 20:08:32 +0700

    TEST 2_fix code

--- Angular-SpringBoot-REST-JWT-master/src/test/java/automation/api/syfgysgdysgd.java
+++ Angular-SpringBoot-REST-JWT-master/src/test/java/automation/api/syfgysgdysgd.java
@@ -2,6 +2,6 @@ package automation.api;
 
 public class syfgysgdysgd {
   public void abc(){
-    System.out.println("Nhi đẹp gái và hiền lành");
+    System.out.println("Nhi đẹp gái, hiền lành và tốt bụng");
   }
 }
